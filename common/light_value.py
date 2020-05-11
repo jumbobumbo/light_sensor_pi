8a769163edc90c70e1e994a0a8165c974a2f9cdd
jumbobumbo <dwheels91@gmail.com>
2020-05-11 19:46:12 +0100

    light_value linted

--- common/light_value.py
+++ common/light_value.py
@@ -3,9 +3,21 @@ from time import sleep
 
 
 class LightValueList:
-    def __init__(self, gpio_pin: int = 7, no_vals: int = 100):
+    """
+    MUST UTILISE 'WITH' TO FUNCTION
+    """
+
+    def __init__(self, gpio_pin: int = 7):
+        """
+        Requires an LDR and capacitor to be wired in to the breadboard
+        Guide, and core of code from here:
+            https://pimylifeup.com/raspberry-pi-light-sensor/
+            Check it out!
+
+        Keyword Arguments:
+            gpio_pin {int} -- GPIO pin number (default: {7})
+        """
         self.gpio_pin = gpio_pin
-        self.no_vals = no_vals
         self.gpio = GPIO
 
     def __enter__(self):
@@ -15,8 +27,14 @@ class LightValueList:
     def __exit__(self, exc_type, exc_val, exc_tb):
         self.gpio.cleanup()
 
-    def return_light_value(self):
-        count = 0
+    def return_light_value(self) -> int:
+        """
+        The higher the count, the darker it is.
+        'Normal' values at different light levels will vary from pi to pi
+
+        Returns:
+            int -- the number of times it took to go round the while loop
+        """
         # set pin to output
         self.gpio.setup(self.gpio_pin, self.gpio.OUT)
         self.gpio.output(self.gpio_pin, self.gpio.LOW)
@@ -27,13 +45,14 @@ class LightValueList:
         self.gpio.setup(self.gpio_pin, self.gpio.IN)
 
         # add to count until pin value is high
+        count = 0
         while (self.gpio.input(self.gpio_pin) == self.gpio.LOW):
             count += 1
 
         return count
 
+
 if __name__ == "__main__":
     with LightValueList() as lightvals:
         for i in range(0, 10):
             print(lightvals.return_light_value())
-            sleep(0.1)
