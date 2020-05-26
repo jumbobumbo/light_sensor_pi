f1f2bc37861b87041c9a19e561fc7df066c66bb3
David Wheeler <dwheels91@gmail.com>
2020-05-26 19:14:46 +0000

    added get

--- flask_app/flask_app.py
+++ flask_app/flask_app.py
@@ -12,10 +12,10 @@ def test_flask() -> str:
     simply for test
     :return: str
     """
-    return "<h1>Pages:</h1><p>tplbulb_set</p>"
+    return "<h1>Pages:</h1><p>tplbulb_set</p><p>tplbulb_get</p>"
 
 @app.route("/tplbulb_set/", methods=["POST"])
-def tpl_bulb_set() -> str:
+def tpl_bulb_set() -> dict:
     """
     controls TPLlight bulbs
     Expected JSON args: MORE TO COME
@@ -29,13 +29,13 @@ def tpl_bulb_set() -> str:
     }
 
     Returns:
-        str -- meaningless string
+        dict: returns the bulb properties dict
     """
     post_data = request.get_json()
 
     # basic verification to check 'ip' key is present
-    if "ip" not in post_data:
-        raise KeyError("An IP must be given to create a bulb connection")
+    if key_validator(["ip"], post_data):
+        return "An IP must be given to create a bulb connection"
 
     # connect to bulb
     with TPL(post_data["ip"]) as bulb:
@@ -54,6 +54,45 @@ def tpl_bulb_set() -> str:
         return_data = bulb.__dict__
 
     return return_data
+
+@app.route("/tplbulb_get/", methods=["POST"])
+def tpl_bulb_get() -> dict:
+    """
+    Allows you to get without setting anything
+
+    Returns:
+        dict: returns the bulb properties dict
+    """
+    post_data = request.get_json()
+
+    # basic verification to check 'ip' key is present
+    if key_validator(["ip"], post_data):
+        return "An IP must be given to create a bulb connection"
+
+    with TPL(post_data["ip"]) as bulb:
+        return bulb.__dict__
+
+
+def key_validator(keys: list, data: dict) -> list:
+    """
+    Returns a list of keys that aren't present in the data dict
+    If all are present, returns nothing
+
+    Arguments:
+        keys {list} -- list of keys
+        data {dict} -- dict to verify
+
+    Returns:
+        list/ None
+    """
+    return_list = []
+    for key in keys:
+        if key not in data.keys():
+            return_list.append(key)
+
+    if len(return_list) > 0:
+        return return_list
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8082)
